39709d503cfa59c5ca41a6c478ef7f71972e92fe
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-02-27 10:37:09 +0100

    Add project files.

---
+++ BowlingConsoleApp/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingConsoleApp
+{
+	public class Game
+	{
+		private int[] rolls = new int[21]; // array på 21 kast. plus en for homer cenario
+		private int currentRoll = 0;
+
+		public void Roll(int pins)
+		{
+			rolls[currentRoll++] = pins;
+		}
+
+	
+		public int Score()
+		{
+			int score = 0;
+			int rollIndex = 0;
+
+			for (int frame = 0; frame < 10; frame++) 
+			{
+				// strike
+				if (IsStrike(rollIndex))
+				{
+					score += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+					rollIndex ++;
+				}
+				// spare
+				else if (IsSpare(rollIndex))
+				{
+					score += 10 + rolls[rollIndex + 2];
+					rollIndex += 2;
+				}
+				else
+				{
+					score += rolls[rollIndex] + rolls[rollIndex + 1];
+					rollIndex += 2;
+				}
+			}
+			return score;
+		}
+
+		private bool IsStrike(int rollIndex)
+		{
+			return rolls[rollIndex] == 10; // hvis et kast er 10, true
+		}
+
+		private bool IsSpare(int rollIndex) 
+		{ 
+			return rolls[rollIndex] + rolls[rollIndex + 1] == 10; // hvis et frame er lig 10, true
+		}
+	}
+}
---
+++ XUnitBowling/UnitTest1.cs
@@ -0,0 +1,87 @@
+using BowlingConsoleApp;
+
+namespace XUnitBowling
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test_Gutter()
+		{
+			Game g = new Game();
+			for (int i = 0; i < 20; i++) 
+			{
+				g.Roll(0);
+			}
+
+			Assert.Equal(0, g.Score());
+		}
+
+		[Fact]
+		public void Test_AllOnes()
+		{
+			Game g = new Game();
+			for (int i = 0; i < 20; i++)
+			{
+				g.Roll(1);
+			}
+
+			Assert.Equal(20, g.Score());
+		}
+
+		[Fact]
+		public void Test_SpareFirst_then3_then0()
+		{
+			Game g = new Game();
+
+			//spare
+			g.Roll(7);
+			g.Roll(3);
+
+			//3 pins
+			g.Roll(3);
+			g.Roll(0);
+
+			for (int i = 0; i < 16; i++)
+			{
+				g.Roll(0);
+			}
+
+			Assert.Equal(16, g.Score());
+		}
+
+		[Fact]
+		public void Test_Strike_then4_then0()
+		{
+			Game g = new Game();
+
+			//strike
+			g.Roll(10);
+
+			//3 pins
+			g.Roll(3);
+			//4 pins
+			g.Roll(4);
+
+			for(int i = 0;i < 17; i++)
+			{
+				g.Roll(0);
+			}
+
+			Assert.Equal(24, g.Score());
+		}
+
+
+		[Fact]
+		public void Test_Homer()
+		{
+			Game g = new Game();
+
+			for(int i = 0;i < 12 ; i++)
+			{
+				g.Roll(10);
+			}
+
+			Assert.Equal(300, g.Score());
+		}
+	}
+}
